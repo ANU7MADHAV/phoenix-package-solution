b872674bf5632918cd527e4962226dee7dae03bc
ANU7MADHAV <anumadhavan888@gmail>
2024-06-24 08:25:47 +0530

    Feat: checking login response

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,9 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const data = await req.json();
-    console.log(data);
-    return NextResponse.json({ message: data, sucess: true });
+    const { email, password } = await req.json();
+    console.log("email", email);
+    console.log("password", password);
+    if (email === process.env.EMAIL && password === process.env.PASS) {
+      console.log("success");
+      return NextResponse.json({ message: "Logged", success: true });
+    }
+    return NextResponse.json({ message: "Failed to logg" });
   } catch (error) {
     console.error(error);
     return NextResponse.error();
--- components/admin/Login.tsx
+++ components/admin/Login.tsx
@@ -1,26 +1,49 @@
 "use client";
+import { signInValidation } from "@/utils/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosResponse } from "axios";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { FormEvent } from "react";
+interface LoginResponse {
+  success: boolean;
+  data: any;
+  message?: string;
+}
+
+type ValidationSchema = z.infer<typeof signInValidation>;
 
 const Login = () => {
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-
-    const submitData = {
-      email: formData.get("email") as string,
-      password: formData.get("password") as string,
+  const router = useRouter();
+  const { handleSubmit } = useForm<ValidationSchema>({
+    resolver: zodResolver(signInValidation),
+  });
+  const onSubmit: SubmitHandler<ValidationSchema> = (submitData) => {
+    const fetchData = async () => {
+      try {
+        const res: AxiosResponse<LoginResponse> = await axios.post(
+          "/api/login",
+          submitData
+        );
+        console.log("hello");
+        console.log(res);
+        console.log("data", res.data.message);
+        if (res.data?.message === "Logged") {
+          console.log("yes");
+        }
+      } catch (error) {
+        console.log(error);
+      }
     };
-
-    console.log(submitData);
+    fetchData();
   };
+
   return (
-    <div className="flex flex-col justify-center items-center rounde shadow-lg border p-5">
+    <div>
       <form
-        className="flex flex-col gap-y-2 justify-center"
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-center items-center rounde shadow-lg border p-5"
       >
         <label htmlFor="email">Email</label>
         <input
@@ -36,10 +59,7 @@ const Login = () => {
           name="password"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2 py-1"
         />
-        <input
-          type="submit"
-          className="bg-blue-500 text-white  px-2 py-1 rounded-md text-center"
-        />
+        <input type="submit" className="bg-blue-500 rounded-lg" />
       </form>
     </div>
   );
